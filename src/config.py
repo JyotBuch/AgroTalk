4cc81ae4193f49ad15c24e21663105ac84ae1f3c
JyotBuch <bjyot2012@gmail.com>
2025-03-01 09:56:13 -0600

    Adding AgroTalk API

--- src/config.py
+++ src/config.py
@@ -4,3 +4,5 @@ Configuration file for the project
 
 # Set your OpenWeatherMap API key here
 ACCUWEATHER_API_KEY = "HjopCPpnztdMziBtiOOToA8PGGYZ50oC"
+
+AgroTalk_API_KEY = "uc_72f58b54966b4f8f956cfbc535b802f7"
