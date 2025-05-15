5b734cdf2a4366777d52793a092694920b511610
Jyot Buch <66951688+JyotBuch@users.noreply.github.com>
2025-05-15 18:48:29 -0500

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,11 +2,11 @@
 Configuration file for the project
 '''
 # Set your OpenWeatherMap API key here
-ACCUWEATHER_API_KEY = "HjopCPpnztdMziBtiOOToA8PGGYZ50oC"
+ACCUWEATHER_API_KEY = "YOUR_API_KEY"
 
 # Set your AgroTalk API key here
-AgroTalk_API_KEY = "uc_72f58b54966b4f8f956cfbc535b802f7"
+AgroTalk_API_KEY = "YOUR_API_KEY"
 
 # Set your Azure OpenAI API key and endpoint here
-AZURE_OPENAI_API_KEY = "DWXVJ9PhzqXbwdJMicfbOAjY4aHKb7Mvjiy0qukDrccEAviT9q67JQQJ99BAACYeBjFXJ3w3AAABACOG0CsL"
-AZURE_OPENAI_ENDPOINT = "https://openai-agco-poc.openai.azure.com"
+AZURE_OPENAI_API_KEY = "YOUR_API_KEY"
+AZURE_OPENAI_ENDPOINT = "YOUR_API_KEY"
